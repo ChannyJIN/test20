4465efa299af2b56b3196bd1454f0918f7617e7e
ChannyJIN <118405988+ChannyJIN@users.noreply.github.com>
2023-10-07 23:19:00 +0900

    second2

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\study\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\study\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app"
+  "FLUTTER_ROOT=C:\\study\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app"
+  "FLUTTER_TARGET=C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\power\\OneDrive\\바탕 화면\\Study\\flutter_VS\\health_app\\.dart_tool\\package_config.json"
+)
